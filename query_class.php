e5269a3ddf64dc8c0408ce80fb60108f053590c7
Your Name <you@example.com>
2018-06-10 09:21:50 -0700

    forgot to commit the query_class.

---
+++ query_class.php
@@ -0,0 +1,40 @@
+<?php
+
+//SQL QUERIES
+$genres = "SELECT DISTINCT Genre FROM games_DOS";
+$home_list = "SELECT Title, Pic, Description, GameFile, ID FROM games_DOS Where ID IN (1393, 1389, 1390, 1391, 1392, 849)";
+$games_by_category = "SELECT Title, Pic, Description, GameFile, ID FROM games_DOS WHERE Genre = '" . $_GET['cat'] . "'";
+
+class Query {
+
+  private $conn;
+
+   public function __construct($conn) {
+     $this->conn = $conn;
+   }
+
+   /***
+   *PAGES: HOME
+   */
+   function get_home_list() {
+    global $conn, $home_list;
+     return mysqli_query($conn,$home_list);
+   }
+
+  /***
+  *PAGES: HOME, CATEGORIES
+  */
+  //GRAB GENRE
+  function grab_genres() {
+    global $conn, $genres;
+    return mysqli_query($conn,$genres);
+  }
+
+  //GET GAME LIST BY CATEGORY
+  function get_games_by_category() {
+      global $conn, $games_by_category;
+      return mysqli_query($conn,$games_by_category);
+    }
+}
+
+?>
